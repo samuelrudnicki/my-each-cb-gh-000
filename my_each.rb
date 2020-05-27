687daabaeb11945ca8f603c911f65009d7d438aa
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-27 17:50:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
+    i=0
+    while i<collection.length
+      yield collection[i]
+      i += 1
+    end
+    collection
+  else
+    "No block given!"
+  end
 end
